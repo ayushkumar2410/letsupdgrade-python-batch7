c788e34e9b8fc20d3ef5aa0d5cbe156cfc52f260
ayushkumar2410 <70808467+ayushkumar2410@users.noreply.github.com>
2020-09-06 21:53:57 +0530

    prime no. of 1 to 100

---
+++ prime no..py
@@ -0,0 +1,8 @@
+for Number in range (1, 101):
+    counting = 0
+    for i in range(2, (Number//2 + 1)):
+        if(Number % i == 0):
+            counting = counting + 1
+
+    if (counting == 0 and Number != 1):
+        print(" %d" %Number, end = '  ')
