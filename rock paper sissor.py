48bda3254dd3b28d19b02a0c1fe8062dc23d4449
ayushkumar2410 <70808467+ayushkumar2410@users.noreply.github.com>
2020-09-23 02:12:29 -0700

    rock paper sissor

---
+++ rock paper sissor.py
@@ -0,0 +1,72 @@
+print("--------------------ROCK--PAPER--SCISSORS-----------------")
+print()
+print("_______________rules of the game are pretty simple____________")
+print()
+print("-----------------r=ROCK---p=PAPER---s=SCISSORS------------------")
+print()
+
+
+def playerinput():
+    player = (input("enter your choice from[ r | p | s ]="))
+
+
+
+def computerinput():
+    lst=["r","p","s"]
+    import random
+    computer = random.choice(lst)
+    print(computer)
+
+
+
+if (computerinput() = "r" and playerinput() = "r"):
+    print("draw")
+elif
+    (computerinput() = "r" and playerinput() = "p"):
+        print("player")
+elif
+    (computerinput() = "r" and playerinput() = "s"):
+        print("computer")
+elif
+    (computerinput() = "p" and playerinput() = "r"):
+        print("computer")
+elif
+    (computerinput() = "p" and playerinput() = "p"):
+        print("draw")
+elif
+    (computerinput() = "p" and playerinput() = "s"):
+        print("player")
+elif
+    (computerinput() = "s" and playerinput() = "r"):
+        print("player")
+elif
+    (computerinput() = "s" and playerinput() = "p"):
+        print("computer")
+else:
+(computerinput() = "s" and playerinput() = "s"):
+        print("draw")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
